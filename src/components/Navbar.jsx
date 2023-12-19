a64d5034f5fb1b3d0429696686d3c9d6d0e32eb3
stefanpython <stefanhtml@yahoo.com>
2023-12-19 20:41:32 +0200

    Style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,26 +15,34 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white-800 p-3 rounded shadow bg-slate-100">
-      <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
-        {/* Desktop View */}
-        <div className="hidden lg:flex items-center space-x-4">
-          {/* Logo */}
-          <div className="text-black text-lg font-bold">Your Logo</div>
+    <nav className="bg-white-800 p-3 shadow bg-slate-100">
+      <div className="container mx-auto flex justify-between items-center flex-wrap">
+        <div className="flex items-center space-x-4 justify-center flex-grow mr-4 sm:justify-start sm:mr-0 sm:flex-grow-0">
+          <div className="text-black text-lg font-bold">Ecom Express</div>
+        </div>
 
-          {/* Search input */}
-          <div className="flex items-center">
-            <input
-              type="text"
-              placeholder="Search products"
-              className="bg-white-700 text-white p-2 rounded items-center"
-            />
+        <div className="flex space-x-4 flex-grow justify-center pt-3 sm:pt-0 sm:flex-grow-0">
+          <input
+            type="text"
+            placeholder="Search products"
+            className="bg-white-700 text-white p-2 rounded items-center w-full"
+          />
+        </div>
+
+        <div className="flex space-x-2 flex-grow justify-between pt-5 sm:pt-0 sm:flex-grow-0">
+          <div className="flex space-x-4">
+            <button className="text-black cursor-pointer">
+              <img className="w-6 h-6" src="./cart.png" alt="cart" />
+              <span className="bg-red-500 text-white rounded-full  w-5 h-5 absolute  -mt-8 ml-4 flex items-center justify-center">
+                3
+              </span>
+            </button>
           </div>
 
-          {/* Cart button */}
-          <button className="text-black cursor-pointer">Cart</button>
+          <div className="flex justify-end flex-grow pl-2">
+            <button className="text-black cursor-pointer">Shop</button>
+          </div>
 
-          {/* Shop and Welcome buttons */}
           <div className="relative">
             <button
               onClick={handleDropdownToggle}
@@ -42,6 +50,7 @@ const Navbar = () => {
             >
               Welcome, {isUserLoggedIn ? "User" : "Guest"}
             </button>
+
             {isDropdownOpen && (
               <div className="absolute top-10 right-0 bg-white p-4 rounded shadow">
                 {isUserLoggedIn ? (
@@ -60,57 +69,6 @@ const Navbar = () => {
               </div>
             )}
           </div>
-
-          {/* Shop button */}
-          <button className="text-black cursor-pointer">Shop</button>
-        </div>
-
-        {/* Mobile View */}
-        <div className="lg:hidden flex flex-col items-center text-center">
-          {/* Logo centered */}
-          <div className="text-black text-lg font-bold mb-3">Your Logo</div>
-
-          {/* Search input */}
-          <div className="mb-3">
-            <input
-              type="text"
-              placeholder="Search products"
-              className="bg-white-700 text-white p-2 rounded items-center"
-            />
-          </div>
-
-          {/* Shop and Welcome buttons */}
-          <div className="mb-3 space-y-2">
-            <button className="text-black cursor-pointer">Shop</button>
-            <div className="relative">
-              <button
-                onClick={handleDropdownToggle}
-                className="text-black cursor-pointer"
-              >
-                Welcome, {isUserLoggedIn ? "User" : "Guest"}
-              </button>
-              {isDropdownOpen && (
-                <div className="absolute top-10 right-0 bg-white p-4 rounded shadow">
-                  {isUserLoggedIn ? (
-                    <>
-                      <div>Dashboard</div>
-                      <div className="cursor-pointer" onClick={handleSignOut}>
-                        Sign Out
-                      </div>
-                    </>
-                  ) : (
-                    <>
-                      <div>Login</div>
-                      <div>Sign Up</div>
-                    </>
-                  )}
-                </div>
-              )}
-            </div>
-          </div>
-
-          {/* Cart button */}
-          <button className="text-black cursor-pointer">Cart</button>
         </div>
       </div>
     </nav>

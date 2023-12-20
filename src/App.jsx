4ccd02bee3ed9e950d1e3f3509c437febbab1385
stefanpython <stefanhtml@yahoo.com>
2023-12-20 19:53:21 +0200

    Position boxes grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,26 @@
+const Homepage = () => {
+  return (
+    <div className="container mx-auto px-4">
+      <div className="grid grid-cols-4 md:grid-cols-5 grid-rows-6 gap-4 py-4">
+        <div className="bg-blue-500 text-white p-4 sm:col-start-1 sm:col-end-2 sm:row-start-1 sm:row-end-3">
+          Box 1
+        </div>
+        <div className="bg-green-500 text-white p-4 sm:col-start-5 sm:col-end-6 sm:row-start-1 sm:row-end-3">
+          Box 2
+        </div>
+        <div className="bg-red-500 text-white p-4 sm:col-start-2 sm:col-end-5 sm:row-start-1 sm:row-end-4">
+          Box 3
+        </div>
+
+        <div className="bg-yellow-500 text-white p-4 sm:col-start-1 sm:col-end-2 sm:h-40 sm:row-start-3 ">
+          Box 4
+        </div>
+        <div className="bg-indigo-500 text-white p-4 sm:col-start-5 sm:col-end-6 sm:h-40 sm:row-start-3 ">
+          Box 5
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;

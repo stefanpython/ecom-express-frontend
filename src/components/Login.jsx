ed846e14f55db0fd1b5ceb078ab09debf0d29ed2
stefanpython <stefanhtml@yahoo.com>
2023-12-28 17:24:56 +0200

    Implement client-side validation for login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,43 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "username") {
+      setUsername(value);
+      setUsernameError("");
+    } else if (name === "password") {
+      setPassword(value);
+      setPasswordError("");
+    }
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // Check for empty username
+    if (!username.trim()) {
+      setUsernameError("Username is required");
+    }
+
+    // Check for empty password
+    if (!password.trim()) {
+      setPasswordError("Password is required");
+    }
+
+    // Perform login logic if both username and password are provided
+    if (username.trim() && password.trim()) {
+      // Add your login logic here
+      console.log("Logging in...");
+    }
+  };
+
   return (
     <div className="flex items-center justify-center min-h-[879px]">
       <div className="-mt-48">
@@ -16,11 +53,21 @@ const Login = () => {
               </label>
 
               <input
-                className="border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-blue-500"
+                className={`border ${
+                  usernameError ? "border-red-500" : "border-gray-300"
+                } rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-blue-500`}
                 id="username"
                 type="text"
+                name="username"
+                value={username}
+                onChange={handleInputChange}
                 placeholder="Enter your username"
               />
+              {usernameError && (
+                <p className="text-red-500 text-sm text-left">
+                  {usernameError}
+                </p>
+              )}
             </div>
 
             <div className="mb-4">
@@ -32,15 +79,26 @@ const Login = () => {
               </label>
 
               <input
-                className="border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-blue-500"
+                className={`border ${
+                  passwordError ? "border-red-500" : "border-gray-300"
+                } rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-blue-500`}
                 id="password"
                 type="password"
+                name="password"
+                value={password}
+                onChange={handleInputChange}
                 placeholder="Enter your password"
               />
+              {passwordError && (
+                <p className="text-red-500 text-sm text-left">
+                  {passwordError}
+                </p>
+              )}
             </div>
 
             <div className="flex justify-between items-center mb-4">
               <button
+                onClick={handleLogin}
                 className="bg-teal-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
                 type="submit"
               >

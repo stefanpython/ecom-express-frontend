fff4f6e8929f877766470f27ab05f023bae9f188
stefanpython <stefanhtml@yahoo.com>
2024-01-05 09:18:19 +0200

    Add images to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,23 +1,50 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 const Homepage = () => {
   return (
-    <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 sm:grid-cols-5 sm:grid-rows-5 gap-4 pt-3 sm:-mb-40">
-        <div className="bg-blue-500 text-white p-4 sm:col-start-1 sm:col-end-2 h-60 sm:h-auto sm:row-start-1 sm:row-end-3">
-          Box 1
-        </div>
-        <div className="bg-green-500 text-white p-4 sm:col-start-5 sm:col-end-6 h-60 sm:h-auto sm:row-start-1 sm:row-end-3">
-          Box 2
-        </div>
-        <div className="bg-red-500 text-white p-4 sm:col-start-2 sm:col-end-5 h-60 sm:h-auto sm:row-start-1 sm:row-end-4">
-          Box 3
-        </div>
+    <div className="container mx-auto px-4 sm:mb-44 bg-slate-500">
+      <div className="grid grid-cols-1 lg:grid-cols-5 lg:grid-rows-5 lg:gap-4 pt-3">
+        <img
+          src="./sales1.jpg"
+          alt=""
+          className="w-full text-white p-4 lg:col-start-1 lg:col-end-2 h-96  lg:row-start-1 lg:row-end-3"
+        />
 
-        <div className="bg-yellow-500 text-white p-4 sm:col-start-1 sm:col-end-2 h-60 sm:h-40 sm:row-start-3 ">
-          Box 4
-        </div>
-        <div className="bg-indigo-500 text-white p-4 sm:col-start-5 sm:col-end-6 h-60 sm:h-40 sm:row-start-3 ">
-          Box 5
-        </div>
+        <img
+          src="./sales1.jpg"
+          alt=""
+          className="w-full text-white p-4 lg:col-start-5 lg:col-end-6 h-96  lg:row-start-1 lg:row-end-3"
+        />
+
+        <Carousel className=" text-white p-4 lg:col-start-2 lg:col-end-5 lg:row-start-1 lg:row-end-4">
+          <div>
+            <img
+              src="./sale1.jpg"
+              alt="Image 1"
+              className="max-w-full h-full object-cover"
+            />
+          </div>
+          <div>
+            <img
+              src="./sale2.jpg"
+              alt="Image 2"
+              className="max-w-full h-full object-cover"
+            />
+          </div>
+        </Carousel>
+
+        <img
+          src="./store.png"
+          alt=""
+          className="w-full text-white p-4 lg:col-start-1 lg:col-end-2 lg:row-start-3 "
+        />
+
+        <img
+          src="./store.png"
+          alt=""
+          className="w-full text-white p-4 lg:col-start-5 lg:col-end-6 lg:row-start-3 "
+        />
       </div>
     </div>
   );

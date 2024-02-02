6ddf4ce1515beab232968044e69fbd07dd1cc8be
stefanpython <stefanhtml@yahoo.com>
2024-02-02 06:45:40 +0200

    Implement admin component

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import { useState } from "react";
+import AddCategory from "./admin/AddCategory";
+import AddProduct from "./admin/AddProduct";
+import Review from "./admin/Review";
 
 const Admin = () => {
+  const [selectedTab, setSelectedTab] = useState("addProduct");
+
   return (
-    <div className="container mx-auto px-24 sm:px-6 lg:px-44 min-h-[870px]">
-      <h1>admin</h1>
+    <div className="flex h-max sm:flex sm:min-h-[875px]">
+      {/* Left Sidebar with Tabs */}
+      <div className="w-1/4 bg-slate-100 p-4">
+        <h1 className="text-xl mb-6 font-medium">Admin Panel</h1>
+
+        <button
+          onClick={() => setSelectedTab("addProduct")}
+          className={`w-full py-2 border border-gray-300 ${
+            selectedTab === "addProduct" ? "bg-blue-500 text-white" : "bg-white"
+          }`}
+        >
+          Add Product
+        </button>
+
+        <button
+          onClick={() => setSelectedTab("addCategory")}
+          className={`w-full py-2 border border-gray-300 ${
+            selectedTab === "addCategory"
+              ? "bg-blue-500 text-white"
+              : "bg-white"
+          }`}
+        >
+          Add Category
+        </button>
+
+        <button
+          onClick={() => setSelectedTab("review")}
+          className={`w-full py-2 border border-gray-300 ${
+            selectedTab === "review" ? "bg-blue-500 text-white" : "bg-white"
+          }`}
+        >
+          Review
+        </button>
+      </div>
+
+      {/* Right Content Area */}
+      <div className="flex-1 p-4">
+        <h1>Welcome to the Admin panel</h1>
+        {/* Render the selected form based on the selectedTab */}
+        {selectedTab === "addProduct" && <AddProduct />}
+        {selectedTab === "addCategory" && <AddCategory />}
+        {selectedTab === "review" && <Review />}
+      </div>
     </div>
   );
 };

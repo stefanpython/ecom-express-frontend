a735dd38b963577e43e072006544232841cb0d98
stefanpython <stefanhtml@yahoo.com>
2024-02-12 17:01:13 +0200

    Create terms and conditions page

--- src/components/footer/Terms.jsx
+++ src/components/footer/Terms.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 const Terms = () => {
-  return <div>Terms</div>;
+  return (
+    <div className="flex items-center justify-center min-h-[870px] ">
+      <div className="max-w-4xl mx-auto px-6 overflow-y-auto max-h-[610px] sm:-mt-44">
+        <h1 className="text-3xl font-bold mb-6">Terms and Conditions</h1>
+        <p className="text-lg mb-4">
+          Welcome to Ecom Express! These terms and conditions outline the rules
+          and regulations for the use of our website.
+        </p>
+        <h2 className="text-xl font-semibold mb-4">1. Acceptance of Terms</h2>
+        <p className="text-lg mb-4">
+          By accessing this website, you agree to be bound by these terms and
+          conditions, all applicable laws, and regulations, and agree that you
+          are responsible for compliance with any applicable local laws. If you
+          do not agree with any of these terms, you are prohibited from using or
+          accessing this site.
+        </p>
+        <h2 className="text-xl font-semibold mb-4">
+          2. Intellectual Property Rights
+        </h2>
+        <p className="text-lg mb-4">
+          The content, trademarks, and other intellectual property on this
+          website are owned by Ecom Express and protected by copyright and other
+          intellectual property laws. You may not modify, copy, reproduce,
+          republish, upload, post, transmit, or distribute any material from
+          this site without prior written consent.
+        </p>
+        <h2 className="text-xl font-semibold mb-4">
+          3. Limitation of Liability
+        </h2>
+        <p className="text-lg mb-4">
+          In no event shall Ecom Express be liable for any damages arising out
+          of the use or inability to use the materials on our website, even if
+          we have been notified orally or in writing of the possibility of such
+          damage.
+        </p>
+        <h2 className="text-xl font-semibold mb-4">4. Governing Law</h2>
+        <p className="text-lg mb-4">
+          These terms and conditions are governed by and construed in accordance
+          with the laws of your country, and you irrevocably submit to the
+          exclusive jurisdiction of the courts in that location.
+        </p>
+        <p className="text-lg mb-4">
+          By using our website, you agree to these terms and conditions. If you
+          do not agree with these terms, please do not use our website.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Terms;

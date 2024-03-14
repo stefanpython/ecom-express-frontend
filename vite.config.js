10da90674080f8c8e7042edc49027658e92b65d6
stefanpython <stefanhtml@yahoo.com>
2024-03-14 07:54:38 +0200

    Edit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "https://stefanpython.github.io/ecom-express-frontend",
+  base: "https://stefanpython.github.io/ecom-express-frontend",
   plugins: [react()],
   test: {
     globals: true, // Make Vitest globals available in test files
